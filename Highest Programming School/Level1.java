11c1f1321fb4626a4101aa05551ceee9b5e02b58
Broken-cat <36155892+Broken-cat@users.noreply.github.com>
2020-07-31 15:58:52 +0300

    Update Level1.java

--- Highest Programming School/Level1.java
+++ Highest Programming School/Level1.java
@@ -1,8 +1,19 @@
 
 public class Level1 {
 	
+	/* Первые попытки решения задачи оказались неудачными, так как подзабыл, как 
+	работает рекурсия и пытался запихнуть всё в один метод.
+	Для решения задачи решил разбить на подзадачи: вычисление факториала
+	и нахождения первой цифры.	
+	/*
+	
 	static int squirrel(int N) {
+		return Character.getNumericValue(String.valueOf(N).charAt(0));
+	}
+	
+	static int fac(int N) {
 		if (N ==0 )return 1;
-		return Character.getNumericValue(String.valueOf((N * (squirrel(N - 1)))).charAt(0)) ;
+		N = N * (fac(N - 1));
+		return N;
 	}
 }
