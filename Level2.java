1290c46dc0828ec891f1b4deadf2070c286605b6
Broken-cat <36155892+Broken-cat@users.noreply.github.com>
2020-07-31 17:42:28 +0300

    Add files via upload

---
+++ Level2.java
@@ -0,0 +1,16 @@
+
+public class Level2 {
+
+	
+	static int odometer(int [] oksana) {
+		int tmp = 0;
+		for (int i = 0; i < oksana.length; i+=2) {
+			if(i == 0 ){
+				tmp += oksana[i]*(oksana[i+1]);
+			} else {
+				tmp += oksana[i] * (oksana[i+1] - oksana[i-1]);
+			}			
+		}
+		return tmp;
+	}
+}
