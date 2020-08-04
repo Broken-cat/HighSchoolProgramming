1a3aa3eb294068c42b18ecb237fbfd91b664a9e1
Broken-cat <36155892+Broken-cat@users.noreply.github.com>
2020-08-04 14:51:02 +0300

    Add files via upload

---
+++ Highest Programming School/Level4.java
@@ -0,0 +1,46 @@
+
+public class Level4 {
+
+	
+	static int[] MadMax(int N, int[] Tele) {
+		sortDown(N, Tele);
+		return sortUp(N, Tele);
+	}
+
+
+	
+	static int[] sortDown(int N, int[] array) {
+		boolean sorted = false;
+		int temp;
+		while (!sorted) {
+			sorted = true;
+			for (int i = 0; i < N - 1; i++) {
+				if (array[i] > array[i+1]) {
+	                temp = array[i];
+	                array[i] = array[i+1];
+	                array[i+1] = temp;
+	                sorted = false;
+	            }
+			}
+		}
+		return array;
+	}
+	
+	static int[] sortUp(int N, int[] array) {
+		boolean sorted = false;
+		int temp;
+		while (!sorted) {
+			sorted = true;
+			for (int i = N/2; i < N - 1; i++) {
+				if (array[i] < array[i+1]) {
+	                temp = array[i];
+	                array[i] = array[i+1];
+	                array[i+1] = temp;
+	                sorted = false;
+	            }
+			}
+		}
+		return array;
+	
+	}
+}
