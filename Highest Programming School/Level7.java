9c1dc4bdaef92ee5da629f0d73641fbca0f43f70
Broken-cat <36155892+Broken-cat@users.noreply.github.com>
2020-08-17 21:57:17 +0300

    Add files via upload

---
+++ Highest Programming School/Level7.java
@@ -0,0 +1,70 @@
+
+public class Level7 {
+
+	
+	
+	static int[] WordSearch(int len, String s, String subs) {
+
+		int k = 0;
+		s = s.replaceAll("[\\s]{2,}", " ");
+		String[] words = takeWords(s, len);
+		
+		
+		
+		
+		int i = 0;
+		while (i < words.length) {
+			try {
+				int summ = 0;
+				while (summ < len) {
+
+					if (summ < len) {
+						summ += words[i].length();
+						i++;
+					} else {
+						break;
+					}
+
+				}
+				k++;
+			} catch (Exception e) {
+				// TODO: handle exception
+				k++;
+			}
+		}
+		String[] N_words = new String[k];
+		int[] res = new int[k];
+
+		for (int j = 0; j < N_words.length; j++) {
+			if (words[j].equals(subs)) {
+				res[j] = 1;
+			} else {
+				res[j] = 0;
+			}
+		}
+
+		return res;
+	}
+
+	static String[] takeWords(String arr, int len) {
+		String tmp = "";
+		char[] splitWords = arr.toCharArray();
+		
+		int k = 0;
+		for (int i = 0; i < splitWords.length; i++) {
+			tmp += splitWords[i];
+			if(splitWords[i] != ' ') {
+				k++;
+				if (k == len) {
+					tmp+= " ";
+					k = 0;
+				}
+			} else {
+				k = 0;
+			}
+		}
+		
+		String[] words = tmp.split(" ");
+		return words;
+	}
+}
