b577cdbd27b719ecd8d9f26953f3a0433c4d031f
Broken-cat <36155892+Broken-cat@users.noreply.github.com>
2020-08-20 14:47:49 +0300

    Add files via upload

---
+++ Highest Programming School/Level8.java
@@ -0,0 +1,19 @@
+
+public class Level8 {
+
+	static int SumOfThe(int N, int[] data) {
+
+		int tmp = data[N - 1];
+		int summ = 0;
+		for (int i = 0; i < data.length; i++) {
+			if (i != N - 1) {
+				summ += data[i];
+			}
+		}
+		if (summ != tmp) {
+			return SumOfThe(N - 1, data);
+		}
+		return tmp;
+
+	}
+}
