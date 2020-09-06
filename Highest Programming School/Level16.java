813bc1f86c0974d8215ec0f3834a0c52452a4c78
Pirogov <36155892+Broken-cat@users.noreply.github.com>
2020-09-06 17:58:08 +0300

    Update Level16.java

--- Highest Programming School/Level16.java
+++ Highest Programming School/Level16.java
@@ -3,6 +3,7 @@ public class Level1 {
 
 	static int MaximumDiscount(int N, int[] price) {
 		int summ = 0;
+		price = sort(price);
 		for (int i = 0; i < price.length; i++) {
 			if((i+1)%3==0)summ+=price[i];
 			else continue;
@@ -15,7 +16,7 @@ public class Level1 {
 		int n = arr.length;
 		for (int i = 0; i < arr.length - 1; i++)
 			for (int j = 0; j < arr.length - i - 1; j++)
-				if (arr[j] > arr[j + 1]) {
+				if (arr[j] < arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
