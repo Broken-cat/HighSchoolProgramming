6affca021d67eb782e31aa91333f26bd00fede0a
Pirogov <36155892+Broken-cat@users.noreply.github.com>
2020-09-11 23:02:51 +0300

    Update Level19Test.java

--- Highest Programming School/bin/Level19Test.java
+++ Highest Programming School/bin/Level19Test.java
@@ -1,104 +1,44 @@
 
 
+import java.util.*;
+
+
+
 class Level1 {
 
 	static String[] ShopOLAP(int N, String[] items) {
+		Arrays.sort(items);
+
 		int[] prices = takePrices(items);
-		String[] item = new String[items.length];
-
-		for (int i = 0; i < item.length; i++) {
-			item[i] = takeItems(items[i]);
+		String[] tmpitms = new String[items.length];	
+		for (int i = 0; i < tmpitms.length; i++) {
+			tmpitms[i] = takeItems(items[i]);
 		}
-
-
-		int k = takeN(item);
-		String[] delItms = deteleRepits(item, k);
+		int k = takeN(tmpitms);
+		
 		String[] result = new String[k];
 		
-		for (int i = 0; i < delItms.length; i++) {
-			String res = delItms[i];
-			String tmp = delItms[i];
-			int pr = 0;
-			for (int j = 0; j < items.length; j++) {
-				if(tmp.equals(item[j])) {
-					pr += prices[j];
-				}
-			}
-			result[i] = res + " " + String.valueOf(pr);
-		}
-		
-		result = reverseAndSort(result);
-		return result;
-	}
-
-	
-	static String[] reverseAndSort(String[] items) {
-		for (int i = 0; i < items.length; i++) {
-			items[i] = reverse(items[i]);
-		}
-		items = sort(items);
-		
-		for (int i = 0; i < items.length; i++) {
-			items[i] = reverse(items[i]);
-		}
-		
-		return items;
-	}
-	
-	static String[] sort(String[] item) {
-		int len = item.length;
-		int[] prices = new int[len];
-		for (int i = 0; i < prices.length; i++) {
-			prices[i] = Integer.parseInt(item[i].substring(item[i].length() - 1));
-		}
-		
-		for (int i = 0; i < len-1; i++) {
-			for (int j = 0; j < len-1-i; j++) {
-				if(prices[j] < prices[j+1]) {
-					int tmp = prices[j];
-					prices[j] = prices[j+1];
-					prices[j+1] = tmp;
-					String st = item[j];
-					item[j] = item[j+1];
-					item[j+1] = st;
-				}
-			}
-		}
-		return item;
-	}
-	
-	static String reverse(String tmp) {
-		 char[] array = tmp.toCharArray();
-		  String result = "";
-		  for (int i = array.length - 1; i >= 0; i--) {
-		     result = result + array[i];
-		  }
-		return tmp;
-	}
-	
-	
-	static String[] deteleRepits(String[] goods, int k) {
-		String[] result = new String[k];
 		int indx = 0;
-		g: for (int i = 0; i < goods.length; i++) {
-			
-			String tmp = goods[i];
-			if (checkRepit(result, tmp)) {
-				result[indx] = tmp;
-				indx++;
-			}else {
-				
+		for (int i = 0; i < tmpitms.length; i++) {
+			String tmp = tmpitms[i];
+			int pr = prices[i];
+			for (int j = 0; j < items.length; j++) {
+				if (j != i & tmp.equals(tmpitms[j])) {
+					pr += prices[j];
+					i++;
+				}
 			}
+			result[indx] = tmpitms[i] + " " + String.valueOf(pr);
+			indx++;
 		}
+
 		return result;
 	}
 
-	static boolean checkRepit(String[] item, String itm) {
-		for (int i = 0; i < item.length; i++)
-			if (String.valueOf(item[i]).equals(itm))
-				return false;
-		return true;
-	}
+
+
+
+
 
 	static String takeItems(String data) {
 		char[] chrs = data.toCharArray();
