2bf66bf60a0272522620ac83cba527e292d646fb
Pirogov <36155892+Broken-cat@users.noreply.github.com>
2020-09-25 20:16:19 +0300

    Update Level21Test.java

--- Highest Programming School/bin/Level21Test.java
+++ Highest Programming School/bin/Level21Test.java
@@ -1,46 +1,15 @@
-package lvl21;
 
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-import sun.misc.IOUtils;
-
-public class Level21Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String tmp = "нклм";
-		String lll = "вибк";
-		String tmp2 = "вкиб";
-		byte[] arr = tmp.getBytes();
-		byte[] arr2 = tmp2.getBytes();
-		byte[] arr3 = lll.getBytes();
-		System.out.println("вибк".compareTo("викб"));// 9
-
-		System.out.println(lll + " -> " + BiggerGreater(lll));
-		System.out.println(tmp2 + " -> " + BiggerGreater(tmp2));
-		System.out.println(tmp + " -> " + BiggerGreater(tmp));
-		// System.out.println(Arrays.toString(arr3));
-		// System.out.print(Arrays.toString(arr));
-		// System.out.println("");
-		// System.out.print(Arrays.toString(arr2));
-		// System.out.println(BiggerGreater("вибк"));
-	}
+public class Level {
 
 	static Stack<String> steck = new Stack<String>();
-
 	public static Stack<String> removeDuplicates(Stack<String> list) {
 
-		// Create a new ArrayList
 		Stack<String> newList = new Stack<String>();
 
-		// Traverse through the first list
 		for (String element : list) {
-
-			// If this element is not present in newList
-			// then add it
 			if (!newList.contains(element)) {
-
 				newList.add(element);
 			}
 		}
