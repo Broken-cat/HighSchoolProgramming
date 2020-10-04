5e13339fd95c1eb0372e651921fccf192a60b33e
Pirogov <36155892+Broken-cat@users.noreply.github.com>
2020-10-04 16:55:16 +0300

    Add files via upload

---
+++ Highest Programming School/bin/lvl242.java
@@ -0,0 +1,114 @@
+package tmp244;
+
+import java.util.Arrays;
+
+public class lvl24 {
+	static String[] s = { "123456", "234567", "345678", "456789" };
+
+	public static void setS(String[] ss) {
+		s = ss;
+	}
+
+	public static String[] getS() {
+		return s;
+	}
+
+	public static void main(String[] argv) {
+		setS(s);
+		for (int i = 0; i < s.length; i++)
+			System.out.println(s[i]);
+		System.out.println();
+		MatrixTurn(s, 4, 6, 3);
+		String[] t = getS();
+		for (int k = 0; k < t.length; k++)
+			System.out.println(t[k]);
+		System.out.println();
+		
+	}
+	
+	static String[] turn(String Matrix[], int M, int N) {
+		Matrix = getS();
+		String MatrixString = "";
+		for (int i = 0; i < Matrix.length; i++) {
+					MatrixString += Matrix[i];
+		}
+	
+			char[] chars = MatrixString.toCharArray();
+			String[][] MatrixMxN = new String[M][N];
+			int count = 0;
+			for (int i = 0; i < M; i++) {
+				for (int j = 0; j < N; j++) {
+					MatrixMxN[i][j] = String.valueOf(chars[count]);
+					count++;
+				}
+			}
+		
+			int row = 0, col = 0;
+
+			String prev;
+			String curr;
+
+			while (row < M && col < N) {
+
+				if (row  == N || col == M)
+					break;
+
+				prev = MatrixMxN[row + 1][col];
+
+				for (int i = col; i < N; i++) {
+					curr = MatrixMxN[row][i];
+					MatrixMxN[row][i] = prev;
+					prev = curr;
+				}
+				row++;
+
+				for (int i = row; i < M; i++) {
+					curr = MatrixMxN[i][N - 1];
+					MatrixMxN[i][N - 1] = prev;
+					prev = curr;
+				}
+				N--;
+
+				if (row < M) {
+					for (int i = N - 1; i >= col; i--) {
+						curr = MatrixMxN[M - 1][i];
+						MatrixMxN[M - 1][i] = prev;
+						prev = curr;
+					}
+				}
+				M--;
+
+				if (col < N) {
+					for (int i = M - 1; i >= row; i--) {
+						curr = MatrixMxN[i][col];
+						MatrixMxN[i][col] = prev;
+						prev = curr;
+					}
+				}
+				col++;
+
+			}
+			
+		
+		 count = 0;
+		String[] result = new String[Matrix.length];
+		String tmp = "";
+		for (int i = 0; i < MatrixMxN.length; i++) {
+			for (int j = 0; j < MatrixMxN[i].length; j++) {
+				tmp += MatrixMxN[i][j];
+				count++;
+			}
+			result[i] = tmp;
+			tmp = "";
+		}
+		setS(result);
+		return result;
+		}
+	
+	
+
+	static void MatrixTurn(String Matrix[], int M, int N, int T) {
+		setS(Matrix);
+		for(int i = 0; i < T; i++)Matrix = turn(Matrix, M, N);
+	}
+}
