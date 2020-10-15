c1bef76e2112eb080646de06db90128f64acb4fc
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-10-15 10:30:23 +0300

    Update LinkedList.java

--- AlgorithmsDataStructure/LinkedList/LinkedList.java
+++ AlgorithmsDataStructure/LinkedList/LinkedList.java
@@ -1,13 +1,14 @@
 
+
 public class LinkedList {
 	public Node head;
 	public Node tail;
-	
+
 	public LinkedList() {
 		head = null;
 		tail = null;
 	}
-	
+
 	public void display() {
 		System.out.print("List (head ---> tail): ");
 		Node current = this.head;
@@ -16,106 +17,129 @@ public class LinkedList {
 			current = current.next;
 		}
 		System.out.println();
+		showHead();
+		showTail();
 	}
-	
+
 	public void addInTail(Node item) {
-		if(this.head == null)this.head = item;
-		else this.tail.next = item;
+		if (this.head == null)
+			this.head = item;
+		else
+			this.tail.next = item;
 		this.tail = item;
 	}
-	
+
 	public Node find(int value) {
 		Node node = this.head;
 		while (node != null) {
-			if(node.value == value)return node;
+			if (node.value == value)
+				return node;
 			node = node.next;
 		}
 		return null;
 	}
-	
-	public ArrayList<Node> findAll (int _value){
-		if(this.head == null)return null;
+
+	public ArrayList<Node> findAll(int _value) {
+		if (this.head == null)
+			return null;
 		ArrayList<Node> nodes = new ArrayList<Node>();
 		Node current = this.head;
-		while(current != null) {
-			if(current.value == _value) {
+		while (current != null) {
+			if (current.value == _value) {
 				nodes.add(current);
 			}
 			current = current.next;
 		}
 		return nodes;
 	}
-	
+
 	public void showHead() {
 		System.out.println("Head --> " + this.head.value);
 	}
-	
+
+	public void showTail() {
+		System.out.println("Tail --> " + this.tail.value);
+	}
+
 	public boolean remove(int _value) {
-		if(this.head == null)return false;
+		if (this.head == null)
+			return false;
 		Node current = this.head;
-		if(current.value == _value)this.head = current.next;
-		Node previous =  current;
-		while(current != null) {
-			if(current.value != _value) {
+		if (current.value == _value) {
+			this.head = current.next;
+			return true;
+		}
+		Node previous = current;
+		while (current != null) {
+			if (current.value != _value) {
 				previous = current;
 				current = current.next;
 			} else {
+				if(this.tail.value == _value && this.tail == current) 
+					this.tail = previous;
 				current = current.next;
 				previous.next = current;
 				return true;
 			}
 		}
-		return false;	
+		return false;
 	}
-	
-	public void removeAll (int _value) {
-		if(this.head == null)return;
+
+	public void removeAll(int _value) {
+		if (this.head == null)
+			return;
 		Node current = this.head;
-		if(current.value == _value)head = current.next;
-		Node previous =  current;
-		while(current != null) {
-			if(current.value != _value) {
+		Node previous = current;
+		while (current != null) {
+			if (current.value != _value) {
 				previous = current;
 				current = current.next;
 			} else {
+				if (this.head.value == _value && this.head == current)
+					this.head = current.next;
+				if(this.tail.value == _value && this.tail == current)
+					this.tail = previous;
 				current = current.next;
 				previous.next = current;
 			}
 		}
 	}
-	
+
 	public void clear() {
 		Node current = this.head;
-		while(current != null) {
+		while (current != null) {
 			this.head = current.next;
 			current = current.next;
 		}
 	}
-	
+
 	public int count() {
 		Node current = this.head;
 		int count = 0;
-		while(current != null) {
+		while (current != null) {
 			current = current.next;
 			count++;
-		}		
+		}
 		return count;
 	}
-	
+
 	public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
-		if(this.head == null)return;
+		if (this.head == null)
+			return;
 		Node current = this.head;
-		while(current != null) {
-			if(current.value == _nodeAfter.value) {
+		while (current != null) {
+			if (current.value == _nodeAfter.value) {
 				Node tmp = current.next;
 				current.next = _nodeToInsert;
 				_nodeToInsert.next = tmp;
+				if (current == this.tail)
+					this.tail = _nodeToInsert;
 				break;
 			}
 			current = current.next;
 		}
 	}
-	
+
 	public LinkedList summLists(LinkedList _l1, LinkedList _l2) {
 		LinkedList result = new LinkedList();
 		if (_l1.count() == _l2.count()) {
