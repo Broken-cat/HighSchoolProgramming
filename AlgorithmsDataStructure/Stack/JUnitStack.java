64916c914434dedd910a6fb723ab9e1a0da32a0a
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-10-31 19:53:22 +0300

    Update JUnitStack.java

--- AlgorithmsDataStructure/Stack/JUnitStack.java
+++ AlgorithmsDataStructure/Stack/JUnitStack.java
@@ -20,6 +20,7 @@ public class JUnitStack {
 		s.push(new Integer(8));
 		s.push(new String("raz"));
 		s.push(true);
+		s.push(null);
 		s.display();
 		s.pop();
 		s.display();
@@ -52,6 +53,58 @@ public class JUnitStack {
 		s.display();
 	}
 	
+	@Test
+	public void balanced() {
+		Stack s = new Stack<>();
+		assertTrue(s.balanced("(()((())))()"));
+	}
 	
+	      
+	@Test
+	  public void postFix() {
+	  int res = 0;
+	  String ex = "82+5*9+=";
+	  char[] chrs = ex.toCharArray();
+	  Stack<Character> sch = new Stack<Character>();
+  	  Stack<Integer> nums = new Stack<Integer>();
+  	  for(int i = 0; i < chrs.length;i++) {
+  		  if(chrs[i] == '=') {
+  			  res = nums.pop();
+  			  return;
+  		  }
+  		  if(!tryParse(chrs[i])) {
+  			  int a = nums.pop();
+  			  int b = nums.pop();
+  			  switch(chrs[i]) {
+  			  case('+'):
+  				  nums.push(a+b);
+  			  break;
+  			  case('*'):
+  			  		nums.push(a*b);
+  			  break;
+  			  }
+  		  } else {
+  			sch.push(chrs[i]);
+  			nums.push(Character.getNumericValue(chrs[i]));
+  		  }
+	
+      
+      public boolean balanced(String str) {
+    	  char[] chrs = str.toCharArray();
+    	  Stack<Character> st = new Stack<Character>();
+    	  for(int i = 0; i < chrs.length; i++) {
+    		  if(chrs[i] == '(') {
+    			  st.push(chrs[i]);
+    			  continue;
+    		  }
+    		  if(st.size() == 0)return false;
+    		  if(chrs[i] == ')') {
+    			  char poped = st.pop();
+    		  } else {
+    			  return false;
+    		  }
+    	  }
+    	  return true;
+      }
 
 }
