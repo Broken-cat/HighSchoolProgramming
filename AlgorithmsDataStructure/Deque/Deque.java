bb6b4cbcba4c1efe6ba71f7052314b41224b71f2
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-11-03 21:20:13 +0300

    Update Deque.java

--- AlgorithmsDataStructure/Deque/Deque.java
+++ AlgorithmsDataStructure/Deque/Deque.java
@@ -1,43 +1,38 @@
 
-public class Deque<T>
-{
-	
+public class Deque<T> {
+
 	private LinkedList<T> dequ;
 	private int count;
 	public T head;
 	public T tail;
-	
-    public Deque()
-    {
-    	dequ = new LinkedList<>();
-    	count = 0;
-    	head = null;
-    	tail = null;
-    }
 
-    public void addFront(T item)
-    {
-    	dequ.addLast(item);
-    	head = dequ.peekLast();
-    	count++;
-    }
+	public Deque() {
+		dequ = new LinkedList<>();
+		count = 0;
+		head = null;
+		tail = null;
+	}
 
-    public void addTail(T item)
-    {
-    	dequ.addFirst(item);
-    	tail = dequ.peekFirst();
-    	count++;
-    }
+	public void addFront(T item) {
+		dequ.addFirst(item);
+		head = dequ.peekFirst();
+		count++;
+	}
 
- 	public T removeFront() {
+	public void addTail(T item) {
+		dequ.addLast(item);
+		tail = dequ.peekLast();
+		count++;
+	}
+
+	public T removeFront() {
 		if (count > 0) {
 			T res = dequ.peekFirst();
 			dequ.removeFirst();
-			tail = dequ.peekFirst();
+			head = dequ.peekFirst();
 			count--;
 			return res;
 		}
-
 		return null;
 	}
 
@@ -45,15 +40,14 @@ public class Deque<T>
 		if (count > 0) {
 			T res = dequ.peekLast();
 			dequ.removeLast();
-			head = dequ.peekLast();
+			tail = dequ.peekLast();
 			count--;
 			return res;
 		}
 		return null;
 	}
-        
-    public int size()
-    {
-     return count; 
-    }
+
+	public int size() {
+		return count;
+	}
 }
