4b2959b664068d5f3232e4ee7a685ba61676ccb2
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-11-12 17:53:47 +0300

    Update HashTable.java

--- AlgorithmsDataStructure/HashTable/HashTable.java
+++ AlgorithmsDataStructure/HashTable/HashTable.java
@@ -1,9 +1,10 @@
 
+
 public class HashTable {
 	public int size;
 	public int step;
 	public String[] slots;
-
+	
 	public HashTable(int sz, int stp) {
 		size = sz;
 		step = stp;
@@ -31,33 +32,33 @@ public class HashTable {
 	}
 
 	public int seekSlot(String value) {
-		if(isFilled())return -1;
+		if(isFilled())
+			return -1;
 		int indx = hashFun(value);
 		if (slots[step+indx] == null || slots[step+indx].equals(value))
-			return indx;
-		else {
-			try {
-				if(step == 0)step = 1;
-				else step *= 2;
-				seekSlot(value);
-			} catch (ArrayIndexOutOfBoundsException e) {
-				// TODO: handle exception
-				step = 0;
-				seekSlot(value);
-			}
 			return indx+step;
+		else {
+			step = 0;
+			while(true) {
+				step++;
+				indx = step % size;
+				if(slots[indx] == null)break;
+			}
+			return indx;
 		}
 	}
+	
 
 	public int put(String value) {
 		step = 0;
-		if(isFilled())
-		return -1;
-		else { 
 			int indx = seekSlot(value);
-			slots[indx] = value;
+			try {
+				slots[indx] = value;
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 			return indx;
-		}		
+			
 	}
 
 	public int find(String value) {
