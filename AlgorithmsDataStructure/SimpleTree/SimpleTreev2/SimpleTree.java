25975b96f153e6217434fd615662e08b4388f3df
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-12-05 17:32:13 +0300

    Update SimpleTree.java

--- AlgorithmsDataStructure/SimpleTree/SimpleTreev2/SimpleTree.java
+++ AlgorithmsDataStructure/SimpleTree/SimpleTreev2/SimpleTree.java
@@ -3,7 +3,6 @@ package SimpleTree;
 import java.util.*;
 
 class SimpleTreeNode<T> {
-	public int NodeLevel;
 	public T NodeValue;
 	public SimpleTreeNode<T> Parent;
 	public List<SimpleTreeNode<T>> Children;
@@ -16,43 +15,112 @@ class SimpleTreeNode<T> {
 }
 
 class SimpleTree<T> {
-	public List<SimpleTreeNode<T>> treeNodes;
 	public SimpleTreeNode<T> Root;
-	public int count;
-	public int Leaf;
 
 	public SimpleTree(SimpleTreeNode<T> root) {
 		Root = root;
-		Root.NodeLevel = 0;
-		treeNodes = new LinkedList<SimpleTreeNode<T>>();
-		count = 1;
-		Leaf = 0;
 	}
-
-	public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild) {
-
-		SimpleTreeNode<T> current = NewChild;
-		if (ParentNode.NodeValue.equals(Root.NodeValue)) {
-			if (Root.Children == null)
-				Root.Children = new LinkedList<SimpleTreeNode<T>>();
-			Root.Children.add(NewChild);
-			current.Parent = Root;
-			current.NodeLevel = Root.NodeLevel + 1;
-			treeNodes.add(NewChild);
-			return;
-		}
-		
-		recSearch(Root, Root.Children, ParentNode, NewChild);
 	
+
+
+	public void recNodeAdd(SimpleTreeNode<T> _startNode, SimpleTreeNode<T> _parent, SimpleTreeNode<T> _nodeToAdd) {
+		if(_startNode.NodeValue.equals(_parent.NodeValue)) {
+			_nodeToAdd.Parent = _startNode;
+			if(_startNode.Children == null)
+				_startNode.Children = new LinkedList<SimpleTreeNode<T>>();
+			_startNode.Children.add(_nodeToAdd);
+		}	
+		else
+		 if(_startNode.Children != null)recListAdd(_startNode.Children, _parent, _nodeToAdd);
+		
 	}
 
-	// getNode
-	public List<SimpleTreeNode<T>> copyChildren(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list,
+	public void recListAdd(List<SimpleTreeNode<T>> list, SimpleTreeNode<T> _parent, SimpleTreeNode<T> _nodeToAdd) {
+		for(int i = 0; i < list.size(); i++) {
+			recNodeAdd(list.get(i), _parent, _nodeToAdd);
+		}
+	}
+	
+	
+	public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild) {
+		if (Root == null) {
+			Root = NewChild;
+			NewChild.Parent = null;
+		} else {
+			recNodeAdd(Root, ParentNode, NewChild);	
+		}
+	}
+	
+	public void recNodeDelete(SimpleTreeNode<T> _startNode,  SimpleTreeNode<T> _nodeToRemove) {
+		 if(_startNode.Children != null)
+			 recListDelete(_startNode.Children,  _nodeToRemove);
+		
+	}
+
+	public void recListDelete(List<SimpleTreeNode<T>> list, SimpleTreeNode<T> _nodeToRemove) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).NodeValue.equals(_nodeToRemove.NodeValue)) {
+				list.remove(i);
+				return;
+			}
+			recNodeDelete(list.get(i), _nodeToRemove);
+		}
+	}
+
+	public void DeleteNode(SimpleTreeNode<T> NodeToDelete) {
+		if(NodeToDelete.NodeValue.equals(Root))return;
+		recNodeDelete(Root, NodeToDelete);
+	}
+	
+	
+	public void recNodeFindBy(SimpleTreeNode<T> _startNode,  T val, List<SimpleTreeNode<T>> _listToAdd) {
+		if(_startNode.NodeValue.equals(val) && _startNode.Children != null) {
+			for(int i = 0; i < _startNode.Children.size(); i++)
+				_listToAdd.add(_startNode.Children.get(i));
+		}
+		 if(_startNode.Children != null)
+			 recListFindBy(_startNode, _startNode.Children,  val, _listToAdd);
+	}
+
+	public void recListFindBy(SimpleTreeNode<T> _startNode, List<SimpleTreeNode<T>> list, T val, List<SimpleTreeNode<T>> _listToAdd) {
+		for(int i = 0; i < list.size(); i++) 
+			recNodeFindBy(list.get(i), val, _listToAdd);
+	}
+	
+	
+	public List<SimpleTreeNode<T>> FindNodesByValue(T val) {
+		List<SimpleTreeNode<T>> res = new LinkedList<SimpleTreeNode<T>>();
+		recNodeFindBy(Root, val, res);
+		if(res.size() == 0)return null;
+		return res;
+	}
+	
+	
+	public void recNodeFindAll(SimpleTreeNode<T> _startNode,   List<SimpleTreeNode<T>> _listToAdd) {
+		 _listToAdd.add(_startNode);
+		 if(_startNode.Children != null)
+			 recListFindAll(_startNode.Children, _listToAdd);
+	}
+
+	public void recListFindAll(List<SimpleTreeNode<T>> list,  List<SimpleTreeNode<T>> _listToAdd) {
+		for(int i = 0; i < list.size(); i++) 
+			recNodeFindAll(list.get(i), _listToAdd);
+	}
+	
+	
+
+	public List<SimpleTreeNode<T>> GetAllNodes() {
+		List<SimpleTreeNode<T>> res = new LinkedList<SimpleTreeNode<T>>();
+		recNodeFindAll(Root, res);
+		return res;
+	}
+
+	public SimpleTreeNode<T> copyChildren(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list,
 			SimpleTreeNode<T> searchingNode) {
 		for (int i = 0; i < list.size(); i++) {
 			SimpleTreeNode<T> res = list.get(i);
 			if (res.NodeValue.equals(searchingNode.NodeValue))
-				return res.Children;
+				return res;
 			if (res.Children != null)
 				copyChildren(res, res.Children, searchingNode);
 
@@ -60,115 +128,22 @@ class SimpleTree<T> {
 		return null;
 	}
 
-	// getNodesadd
-	public void recSearch(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list) {
-		for (int i = 0; i < list.size(); i++) {
-			SimpleTreeNode<T> res = list.get(i);
-			//System.out.println("Taking node: " + res.NodeValue);
-			if (!treeNodes.contains(res))
-				treeNodes.add(res);
-			if (res.Children != null)
-				recSearch(res, res.Children);
-
-		}
-		return;
-	}
-
-	public void leafC(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list) {
-		for (int i = 0; i < list.size(); i++) {
-			SimpleTreeNode<T> res = list.get(i);
-			if (res.Children != null)
-				leafC(res, res.Children);
-			else
-				Leaf++;
-
-		}
-	}
-
-	public void addNode(SimpleTreeNode<T> Parent, SimpleTreeNode<T> Child) {
-		if (Parent.Children == null)
-			Parent.Children = new LinkedList<SimpleTreeNode<T>>();
-		Parent.Children.add(Child);
-		Child.Parent = Parent;
-		Child.NodeLevel = Parent.NodeLevel + 1;
-		// treeNodes.add(Child);
-	}
-
-	// nodeToAdd
-	public void recSearch(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list, SimpleTreeNode<T> searchingParent,
-			SimpleTreeNode<T> NodeToAdd) {
-		for (int i = 0; i < list.size(); i++) {
-			SimpleTreeNode<T> res = list.get(i);
-			if (res.NodeValue.equals(searchingParent.NodeValue)) {
-				addNode(searchingParent, NodeToAdd);
-			}
-			if (res.Children != null) {
-				recSearch(res, res.Children, searchingParent, NodeToAdd);
-			}
-		}
-		return;
-	}
-
-	// deleting
-	public void recSearch(SimpleTreeNode<T> startNode, List<SimpleTreeNode<T>> list, SimpleTreeNode<T> _nodeToRemove) {
-		for (int i = 0; i < list.size(); i++) {
-			SimpleTreeNode<T> res = list.get(i);
-			if (res.NodeValue.equals(_nodeToRemove.NodeValue)) {
-				_nodeToRemove.Children = null;
-				list.remove(_nodeToRemove);
-				treeNodes.remove(_nodeToRemove);
-				return;
-			} else if (res.Children != null) {
-				recSearch(res, res.Children, _nodeToRemove);
-			}
-
-		}
-		return;
-	}
-
-	public void DeleteNode(SimpleTreeNode<T> NodeToDelete) {
-		if (Root.NodeValue.equals(NodeToDelete.NodeValue))
-			return;
-		recSearch(Root, Root.Children, NodeToDelete);
-	}
-
-	public List<SimpleTreeNode<T>> GetAllNodes() {
-		treeNodes.clear();
-		treeNodes.add(Root);
-		recSearch(Root, Root.Children);
-		for (int i = 1; i < treeNodes.size(); i++) {
-			//System.out.print(treeNodes.get(i).NodeValue + " Parent: " + treeNodes.get(i).Parent.NodeValue + " Node Level: " + treeNodes.get(i).NodeLevel);
-			//System.out.println();
-		}
-		return treeNodes;
-	}
-
-	public List<SimpleTreeNode<T>> FindNodesByValue(T val) {
-		List<SimpleTreeNode<T>> res = new LinkedList<SimpleTreeNode<T>>();
-		List<SimpleTreeNode<T>> t = GetAllNodes();
-		for (int i = 0; i < t.size(); i++) {
-			if (val.equals(t.get(i).NodeValue))
-				res.add(t.get(i));
-		}
-		return res;
-	}
-
 	public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent) {
 		SimpleTreeNode<T> tmp = new SimpleTreeNode(OriginalNode.NodeValue, null);
-		tmp.Children = copyChildren(Root, Root.Children, OriginalNode);
-		recSearch(Root, Root.Children, NewParent, tmp);
+		tmp = copyChildren(Root, Root.Children, OriginalNode);
 		DeleteNode(OriginalNode);
+		AddChild(NewParent, tmp);
+		
+	//	
 	}
 
 	public int Count() {
 
-		return treeNodes.size();
+		return 0;
 	}
 
 	public int LeafCount() {
-		Leaf = 0;
-		leafC(Root, Root.Children);
-		return Leaf;
-	}
 
+		return 0;
+	}
 }
