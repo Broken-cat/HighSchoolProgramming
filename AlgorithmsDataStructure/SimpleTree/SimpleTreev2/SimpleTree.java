e464e6f4475429e10d14de186bc551c4d67e9d02
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-12-06 13:44:42 +0300

    Update SimpleTree.java

--- AlgorithmsDataStructure/SimpleTree/SimpleTreev2/SimpleTree.java
+++ AlgorithmsDataStructure/SimpleTree/SimpleTreev2/SimpleTree.java
@@ -59,7 +59,7 @@ class SimpleTree<T> {
 
 	public void recListDelete(List<SimpleTreeNode<T>> list, SimpleTreeNode<T> _nodeToRemove) {
 		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i).NodeValue.equals(_nodeToRemove.NodeValue)) {
+			if(list.get(i).equals(_nodeToRemove)) {
 				list.remove(i);
 				return;
 			}
