bb1228f99a9448d12ee1aef5fafc782f1b62d88f
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2020-12-11 15:57:20 +0300

    Update BST.java

--- AlgorithmsDataStructure/BinaryTree/BST.java
+++ AlgorithmsDataStructure/BinaryTree/BST.java
@@ -1,4 +1,5 @@
 
+
 class BSTNode<T> {
 	public int ChildsCount;
 	public int NodeKey;
@@ -7,7 +8,6 @@ class BSTNode<T> {
 	public BSTNode<T> LeftChild;
 	public BSTNode<T> RightChild;
 	public boolean isLeaf;
-	
 
 	public BSTNode(int key, T val, BSTNode<T> parent) {
 		NodeKey = key;
@@ -17,15 +17,19 @@ class BSTNode<T> {
 		RightChild = null;
 		ChildsCount = 0;
 	}
-	
+
 	public boolean isLeaf() {
-		if(LeftChild == null && RightChild == null) return true;
-		else return false;
+		if (LeftChild == null && RightChild == null)
+			return true;
+		else
+			return false;
 	}
-	
+
 	public int ChildCount() {
-		if(LeftChild == null && RightChild == null) return 0;
-		if(LeftChild != null && RightChild != null) return 2;
+		if (LeftChild == null && RightChild == null)
+			return 0;
+		if (LeftChild != null && RightChild != null)
+			return 2;
 		return 1;
 	}
 }
@@ -46,12 +50,11 @@ class BSTFind<T> {
 public class BST<T> {
 	BSTNode<T> Root;
 	private int count;
+
 	public BST(BSTNode<T> node) {
 		Root = node;
 		count = 0;
 	}
-
-	
 
 	public BSTFind<T> FindNodeByKey(int key) {
 		BSTNode<T> current = Root;
@@ -81,7 +84,7 @@ public class BST<T> {
 
 	public boolean AddKeyValue(int key, T val) {
 		BSTNode<T> nodeToAdd = new BSTNode(key, val, null);
-		if(Root == null) {
+		if (Root == null) {
 			Root = nodeToAdd;
 			return true;
 		}
@@ -120,103 +123,118 @@ public class BST<T> {
 
 	public boolean DeleteNodeByKey(int key) {
 		BSTFind<T> current = FindNodeByKey(key);
-		BSTNode<T> currentNode = current.Node; 
+		if (current.NodeHasKey == false)
+			return false;
+		BSTNode<T> currentNode = current.Node;
 		BSTNode<T> reciever;// = FinMinMax(current.Node, false);
-		if(current.NodeHasKey==false)return false;
-		if(current.Node.isLeaf()) {
-			if(current.Node.Parent.NodeKey > current.Node.NodeKey)
-				current.Node.Parent.LeftChild = null;
-			else
-				current.Node.Parent.RightChild = null;
-			if(current.Node.equals(Root))
+		if (currentNode.ChildCount() == 0) {
+			if(currentNode.equals(Root)) {
+				currentNode = null;
 				Root = null;
-			current.Node.Parent = null;
-			current.Node = null;
-			return true;
-		} else if (current.Node.ChildCount() == 1) {
-			if(current.Node.LeftChild != null) 
-				reciever = current.Node.LeftChild;
-			else
-				reciever = current.Node.RightChild;
-			if(!current.Node.equals(Root)) {
-				if(current.Node.NodeKey > current.Node.Parent.NodeKey) 
-					current.Node.Parent.RightChild = reciever;
-				else
-					current.Node.Parent.LeftChild = reciever;
+				return true;
 			}
-			reciever.Parent = current.Node.Parent;
-			if(current.Node.equals(Root))
-				Root = reciever;
-			current.Node = null;
-			return true;
-		} else {
-			//find a reciever
-			reciever = FinMinMax(current.Node.RightChild, false);
-			
-			//link reciever RightChild with recieverParent
-			if(reciever.RightChild != null) {
-				reciever.RightChild.Parent = reciever.Parent;
-				reciever.Parent.LeftChild = reciever.RightChild;
-			} else if (!reciever.Parent.equals(Root))
-				reciever.Parent.LeftChild = null;
-			
-			if(!reciever.equals(current.Node.LeftChild))
-				reciever.LeftChild = current.Node.LeftChild;
-			if(!reciever.equals(current.Node.RightChild))
-				reciever.RightChild = current.Node.RightChild;
-			if(reciever.NodeKey < reciever.Parent.NodeKey)
-				reciever.Parent.LeftChild = null;
+			if (currentNode.Parent.LeftChild != null && currentNode.Parent.LeftChild.equals(currentNode))
+				currentNode.Parent.LeftChild = null;
 			else
-				reciever.Parent.RightChild = null;
-			
-			//making parent of deleting node reference to reciever 
-			if(!current.Node.equals(Root)){
-				if(current.Node.NodeKey > current.Node.Parent.NodeKey)
-					current.Node.Parent.RightChild = reciever;
-				else
-					current.Node.Parent.LeftChild = reciever;
-			} 
-			
-			if(current.Node.equals(Root)) {
-				if(Root.LeftChild != null)
-					Root.LeftChild.Parent = reciever;
-				if(Root.RightChild != null)
-					Root.RightChild.Parent= reciever;
-				Root = reciever;
-			}	else
-				reciever.Parent = current.Node.Parent;
-			current.Node = null;
+				currentNode.Parent.RightChild = null;
+			currentNode = null;
 			return true;
-			
 		}
-		
+		if (currentNode.ChildCount() == 1) {
+			if (!currentNode.equals(Root)) {
+				if (currentNode.NodeKey > currentNode.Parent.NodeKey) {
+					if (currentNode.LeftChild != null) {
+						currentNode.Parent.RightChild = currentNode.LeftChild;
+						currentNode.LeftChild.Parent = currentNode.Parent;
+					} else {
+						currentNode.Parent.RightChild = currentNode.RightChild;
+						currentNode.RightChild.Parent = currentNode.Parent;
+					}
+				} else {
+					if (currentNode.LeftChild != null) {
+						currentNode.Parent.LeftChild = currentNode.LeftChild;
+						currentNode.LeftChild.Parent = currentNode.Parent;
+					} else {
+						currentNode.Parent.LeftChild = currentNode.RightChild;
+						currentNode.RightChild.Parent = currentNode.Parent;
+					}
+				}
+			} else {
+				if (currentNode.LeftChild != null) {
+					currentNode.LeftChild.Parent = null;
+					Root = currentNode.LeftChild;
+				} else {
+					currentNode.RightChild.Parent = null;
+					Root = currentNode.RightChild;
+				}
+
+			}
+			currentNode = null;
+			return true;
+		} else if (currentNode.ChildCount() == 2) {
+			reciever = FinMinMax(currentNode.RightChild, false);
+			if(reciever.LeftChild == null && reciever.RightChild == null) {
+				reciever.LeftChild = currentNode.LeftChild;
+				reciever.RightChild = currentNode.RightChild;
+				reciever.RightChild = null;
+				if(currentNode.equals(Root)) {
+					Root = reciever;
+				} else {
+					if(reciever.NodeKey > reciever.Parent.NodeKey) {
+						currentNode.Parent.RightChild = reciever;;
+					} else {
+						currentNode.Parent.LeftChild = reciever;
+					}
+				}
+					
+				
+				return true;
+			} else if (reciever.LeftChild == null && reciever.RightChild != null) {
+				reciever.RightChild.Parent = reciever.Parent;
+				if(reciever.NodeKey > reciever.Parent.NodeKey)
+					reciever.Parent.RightChild = reciever.RightChild;
+				else
+					reciever.Parent.LeftChild = reciever.RightChild;
+				reciever = null;
+				return true;
+			} else {
+				if(reciever.RightChild != null) {
+					reciever.RightChild.Parent = reciever.Parent;
+					reciever.Parent.LeftChild = reciever.RightChild;
+				}
+				reciever.LeftChild = currentNode.LeftChild;
+				reciever.RightChild = currentNode.RightChild;
+				if(currentNode.equals(Root))
+					Root = reciever;
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public void Show(BSTNode<T> _startNode) {
-		System.out.println(_startNode.NodeKey);
-		if (_startNode.LeftChild != null) 
+	//	System.out.println(_startNode.NodeKey);
+		if (_startNode.LeftChild != null)
 			Show(_startNode.LeftChild);
-		if (_startNode.RightChild != null) 
-			Show(_startNode.RightChild);	
+		if (_startNode.RightChild != null)
+			Show(_startNode.RightChild);
 	}
-	
-	
-	
+
 	public void recTest(BSTNode<T> _startNode) {
 		System.out.println(_startNode.NodeKey);
-		if(_startNode.LeftChild != null || _startNode.RightChild != null)
+		if (_startNode.LeftChild != null || _startNode.RightChild != null)
 			count++;
-		if (_startNode.LeftChild != null) 
+		if (_startNode.LeftChild != null)
 			recTest(_startNode.LeftChild);
-		if (_startNode.RightChild != null) 
+		if (_startNode.RightChild != null)
 			recTest(_startNode.RightChild);
-			
+
 	}
-	
-	
-	
+
 	public int Count() {
 		count = 0;
+		if(Root == null) return 0;
 		recTest(Root);
 		return count;
 	}
