ae33d871a938ab5a3652f63f4ea7ad2cd0b8b20d
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2021-02-09 05:56:20 -0900

    Update SortLevel.java

--- AlgorithmsDataStructure/SortLevel/SortLevel.java
+++ AlgorithmsDataStructure/SortLevel/SortLevel.java
@@ -1,38 +1,37 @@
 
+
 public class SortLevel {
-	
-	 public static void SelectionSortStep( int array[], int i )
-     {
-       int temp = array[i];
-       int min = findMin(array, i);
-       array[i] = array[min];
-       array[min] = temp;
-     }
-	 
-	 public static int findMin(int array[], int from) {
-		 int min = array[from];
-		 for(int i = from; i < array.length; i++) {
-			 if(array[min] > array[i])
-				 min = i;
-		 }
-		 return min;
-	 }
-	 
-	 public static boolean BubbleSortStep ( int array[] )
-     {
-	   boolean result = false;
-	   for(int i = 0; i < array.length; i+=2) {
-		   if(array[i] > array[i+1]) {
-			   result = true;
-			   int temp = array[i];
-			   array[i] = array[i+1];
-			   array[i+1] = temp;
-		   }
-	   }
-       return result;
-     }
-	
-		public static void InsertionSortStep(int[] array, int step, int i) {
+
+	public static void SelectionSortStep(int array[], int i) {
+		int temp = array[i];
+		int min = findMin(array, i);
+		array[i] = array[min];
+		array[min] = temp;
+	}
+
+	public static int findMin(int array[], int from) {
+		int min = array[from];
+		for (int i = from; i < array.length; i++) {
+			if (array[min] > array[i])
+				min = i;
+		}
+		return min;
+	}
+
+	public static boolean BubbleSortStep(int array[]) {
+		boolean result = false;
+		for (int i = 0; i < array.length; i += 2) {
+			if (array[i] > array[i + 1]) {
+				result = true;
+				int temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+			}
+		}
+		return result;
+	}
+
+	public static void InsertionSortStep(int[] array, int step, int i) {
 			for(int k = i+step; k < array.length; k+=step) {
 				int key = array[k];
 				int ind = k-step;
@@ -42,5 +41,33 @@ public class SortLevel {
 				}
 				array[ind+step]=key;
 			}
+	}
+	
+	public static ArrayList KnuthSequence( int array_size ) {
+		ArrayList sequence = new ArrayList();
+		int n = 1;
+		sequence.add(n);
+		while (3*n+1 < array_size) {
+			n = 3 * n + 1;
+			sequence.add(0, n);
+		}
+		return sequence;
+	}
+	
+	public static void ShellSort(int array_size, int array[]) {
+		ArrayList<Integer> list = KnuthSequence(array_size);
+		for(int i : list) {
+			for(int k = 0; k < i; k++) {
+				InsertionSortStep(array, i, k);
+			}
+		}
+	}
+
+	public static void swap(int i, int j, int array[]) {
+		if (array[i] < array[j]) {
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
 	}
 }
