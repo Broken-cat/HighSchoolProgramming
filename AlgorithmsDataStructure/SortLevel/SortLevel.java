6ce30e29e245cd0d276ad1527e16bb9658c67184
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2021-02-16 08:31:42 -0900

    Update SortLevel.java

--- AlgorithmsDataStructure/SortLevel/SortLevel.java
+++ AlgorithmsDataStructure/SortLevel/SortLevel.java
@@ -233,3 +233,29 @@ public class SortLevel {
 	}
 	
 }
+
+class HeapSort {
+	public Heap HeapObject;
+	
+	public HeapSort(int[] arr) {
+		HeapObject = new Heap();
+		int dep = getDepth(arr.length);
+		HeapObject.MakeHeap(arr, dep);
+	}
+	
+	public int getDepth(int len) {
+		int temp = 0;
+		int index = 0;
+		while(temp < len) {
+			temp += (int) Math.pow(2, index);
+			index++;
+		}
+		return --index;
+	}
+	
+	public int GetNextMax() {
+		while(HeapObject.GetMax() != -1) {
+		}
+		return -1;
+	}
+}
