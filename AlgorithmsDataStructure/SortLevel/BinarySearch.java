bd87aea86e9447ab80ef78443bfcc36d97ab1e9a
Pirogov <36155892+AlexandrPirogov@users.noreply.github.com>
2021-02-18 08:38:11 -0900

    Update BinarySearch.java

--- AlgorithmsDataStructure/SortLevel/BinarySearch.java
+++ AlgorithmsDataStructure/SortLevel/BinarySearch.java
@@ -1,23 +1,37 @@
+
 public class BinarySearch {
 	public int Left, Right;
 	private int[] numbers;
-	private int len;
+	private int processing;
 	
 	public BinarySearch(int arr[]) {
 		numbers = arr;
 		Left = 0;
 		Right = arr.length-1;
+		processing = 0;
 	}
 	
-	public int Step( int N) {
-		if((Left == Right && numbers[Left] != N) ||
-				(Left == Right-1 && numbers[Left] != N))return -1;
-		if(numbers[(Right+Left)/2] == N)return 1;
-		if(numbers[(Right+Left)/2] > N) {
-			Right = Right/2;
-		} else {
-			Left += Right/2;
+	public void Step( int N) {
+		if(processing != 0)return;
+		int middle = (Right+Left)/2;
+		if(numbers[middle] == N) {
+			processing = 1;
+			return;
 		}
-		return 0;
+		if((Left == Right && numbers[Left] != N) ||
+				(Left == Right-1 && numbers[Left] != N)) {
+			processing =  -1;
+			return;
+		}
+		if(numbers[middle] > N) {
+			Right = middle;
+		} else {
+			Left = middle;
+		}
+		processing = 0;
+	}
+	
+	public int GetResult() {
+		return processing;
 	}
 }
